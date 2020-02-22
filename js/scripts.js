1a4abef28c538b4bca3bda8c5a2b8f91a6ec9677
soulkabs <soulkabs@mail.ru>
2020-02-22 14:26:50 +0300

    change main banner, blocks of models, add mobile view in media requests

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 		$(".sx4").fadeOut(600).css("display","");
 		$(".vitara").fadeIn(600).css("display","flex");
 	}); 
-	$("[src='img/image 18.png']").click(function() {
+	$("[src='img/image18.png']").click(function() {
 		$(".suzuki:even").removeClass("suzuki_active");
 		$(".suzuki:odd").addClass("suzuki_active");
 		$(".sx4").fadeIn(600).css("display","flex");
@@ -16,10 +16,10 @@ $(document).ready(function(){
 	$(".suzukiItem__button1").click(function(){$(".about").slideToggle(800)});
 	$(".content__close").click(function(){ $(".about").css("display", "none")});
 	/*-Bunner-*/
-	$(".bunner").click(function(){
+	$(".next__right").click(function(){
 		$(".bunner").fadeOut(600).css("display", "none");
 		$(".bunner2").fadeIn(600).css("display", "block");});
-	$(".bunner2").click(function(){
+	$(".next__left").click(function(){
 		$(".bunner2").fadeOut(600).css("display", "");
 		$(".bunner").fadeIn(600).css("display", "block");});
 });
